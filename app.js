b931cb75a10b7b91f3b4e2d48f5e2e4cd94ca534
nathan sheryak <nathanraysheryak@gmail.com>
2020-11-27 13:12:56 -0500

    get User model and create/read routes

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const sequelize = require('./database/index');
+
 // import models here
+const User = require('./models/user');
 
 const PORT = 3000;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+const userRoutes = require('./routes/user');
+
+app.use('/user', userRoutes);
 
 // put middleware to grab userid here
 
@@ -18,6 +25,8 @@ async function assertDatabaseConnection() {
   try {
     await sequelize.authenticate();
     console.log('db authentication successful');
+    await sequelize.sync({ force: true });
+    console.log('sync successful');
   } catch (error) {
     console.log('db authentication failed: \n ', error.message);
     process.exit(1);
---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.send('welcome to user route'));
+
+router.get('/allUsers', async (req, res) => {
+  const users = await User.findAll();
+  res.status(200).json(users);
+});
+
+router.post('/createUser', async (req, res) => {
+  console.log('req.body ', req.body);
+  const user = await User.create(req.body);
+  res.status(200).json(user);
+});
+
+module.exports = router;
