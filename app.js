6aa67a7099d7c58c613b1049779c8b31799cc2d2
nathan sheryak <nathanraysheryak@gmail.com>
2021-02-10 18:27:38 -0500

    setup express server

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const { Sequelize } = require('sequelize');
+const express = require('express');
+
+const PORT = 3000;
+const app = express();
 
 const sequelize = new Sequelize({
   database: 'sequelize_sandbox_v3',
@@ -8,11 +12,18 @@ const sequelize = new Sequelize({
   dialect: 'postgres',
 });
 
-try {
-  sequelize.authenticate();
-  console.log('connection good');
-} catch (error) {
-  console.log('connection bad ', error);
+async function initializeDatabaseConnection() {
+  try {
+    sequelize.authenticate();
+    console.log('connection good');
+  } catch (error) {
+    console.log('connection bad ', error);
+  }
 }
 
-// Up Next: Finders
+async function init() {
+  await initializeDatabaseConnection();
+  app.listen(PORT, () => console.log(`sequelize_sandbox_v2 running on ${PORT}`));
+}
+
+init();
